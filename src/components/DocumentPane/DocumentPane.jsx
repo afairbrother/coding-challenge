1f14c0c288abfd410c3525d0f02496437b42cd68
Amy Fairbrother <afairbrother@sapient.com>
2016-07-17 14:19:58 -0400

    add components and basic styles

---
+++ src/components/DocumentPane/DocumentPane.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='document-pane'>
+        <h3>DocumentPane</h3>
+      </header>
+    );
+  }
+}
---
+++ src/components/EditorPane/EditorPane.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='editor-pane'>
+        <h3>EditorPane</h3>
+      </header>
+    );
+  }
+}
